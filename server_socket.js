995853542186caed4e7d87ded92e30af21eb3c9f
matteo bandoni <m.bandoni@aksilia.com>
2022-12-14 11:11:30 +0100

    gestione delle room

--- server_socket.js
+++ server_socket.js
@@ -29,30 +29,37 @@ app.get('/', (req, res) => {
 
 
 io_rapporto_audit.on('connection', (socket) => {
-  console.log('new user connected');
+  console.log('Rapporto_audit => new user connected');
+
+  socket.on('join rapporto audit', (id) =>{
+    console.log('Rapporto_audit => mi collego alla room: ' + id);
+    socket.leave(socket.room);
+    socket.join(id);
+    socket.room = id;
+  })
 
   socket.on('cambio risposta default', (id_domanda,risposta,selezionato, data_caricamento) => {
-    socket.broadcast.emit('cambio risposta default', id_domanda, risposta, selezionato, data_caricamento);
+    socket.broadcast.to(socket.room).emit('cambio risposta default', id_domanda, risposta, selezionato, data_caricamento);
   });
 
   socket.on('aggiungi rilievo', (id, text, id_domanda, id_row) => {
-    socket.broadcast.emit('aggiungi rilievo', id, text, id_domanda, id_row);
+    socket.broadcast.to(socket.room).emit('aggiungi rilievo', id, text, id_domanda, id_row);
   });
 
   socket.on('rimuovi rilievo', (id, id_domanda) => {
-    socket.broadcast.emit('rimuovi rilievo', id, id_domanda);
+    socket.broadcast.to(socket.room).emit('rimuovi rilievo', id, id_domanda);
   });
 
   socket.on('typing evidenza', (id_domanda) => {
-    socket.broadcast.emit('typing evidenza', id_domanda);
+    socket.broadcast.to(socket.room).emit('typing evidenza', id_domanda);
   });
 
   socket.on('cambio evidenza', (id_domanda, text, data_caricamento) => {
-    socket.broadcast.emit('cambio evidenza', id_domanda, text, data_caricamento);
+    socket.broadcast.to(socket.room).emit('cambio evidenza', id_domanda, text, data_caricamento);
   });
 
   socket.on('cambio risposta progress',(id_domanda, value, data_caricamento, gruppo, totale) => {
-    socket.broadcast.emit('cambio risposta progress', id_domanda, value, data_caricamento, gruppo, totale);
+    socket.broadcast.to(socket.room).emit('cambio risposta progress', id_domanda, value, data_caricamento, gruppo, totale);
   });
 
 });
